d3637bd78cc12b7f47fa8e533a00230a844a637f
Saqlain Mustak <56887504+SaqlainCSE@users.noreply.github.com>
2021-04-21 23:04:30 +0600

    Implement token refresh functionality using cookie

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::get('/auth/createToken', 'API\Auth\LoginAPIController@createToken');
 Route::group([
     'prefix' => 'auth',
 ], function () {
-    Route::get('/auth/createToken', 'API\Auth\LoginAPIController@createToken'); // just to check
+    Route::get('/auth/createToken', 'API\Auth\LoginAPIController@createToken');
     Route::get('/auth/refreshToken', 'API\Auth\LoginAPIController@refreshToken'); 
     Route::post('login', 'API\Auth\LoginAPIController@login');
     Route::post('register', 'API\Auth\RegisterAPIController@register');
